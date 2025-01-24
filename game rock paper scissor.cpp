07aabb5bb8544534b7e61d6c706123d52ac94cd2
Kunal Dev <kunaldev9142@gmail.com>
2025-01-24 18:50:02 +0530

    Fix, Lines added

--- game rock paper scissor.cpp
+++ game rock paper scissor.cpp
@@ -10,29 +10,30 @@ public:
     string computer_choices;
     body()
     {
-        string choices[] = {"Rock", "Paper", "Scissor"};
+        string choices[] = {"R", "P", "S"};
         int index = rand() % 3;
         computer_choices = choices[index];
-        cout << computer_choices << endl;
+        // cout <<" Computer choice :"<< computer_choices<<endl;
+       
     }
     void function()
     {
         string human_choices;
-        cout << "Choose one: Rock, Paper, or Scissors :";
+        cout << "Choose one: Rock(R), Paper(P), or Scissors(S) :";
         cin >> human_choices;
         if (human_choices == computer_choices)
         {
             cout << "It's a tie!" << endl;
         }
-        else if (human_choices == "Rock" && computer_choices == "Scissors")
+        else if (human_choices == "R" && computer_choices == "S")
         {
             cout << "You win!" << endl;
         }
-        else if (human_choices == "Paper" && computer_choices == "Rock")
+        else if (human_choices == "P" && computer_choices == "R")
         {
             cout << "You win!" << endl;
         }
-        else if (human_choices == "Scissors" && computer_choices == "Paper")
+        else if (human_choices == "S" && computer_choices == "P")
         {
             cout << "You win!" << endl;
         }
@@ -46,7 +47,11 @@ public:
 int main()
 {
     srand(static_cast<unsigned int>(time(0)));
-    cout << "Welcome to Rock-Paper-Scissors!" << endl;
+    cout << "Welcome to Rock Paper Scissors!" << endl;
+    cout <<" Some Basic Symbole"<<endl;
+    cout <<"S Stand  For Scissor"<<endl;
+    cout <<"R Stand  For Rock"<<endl;
+    cout <<"P Stand  For Paper"<<endl;
     char play;
     do
     {
